3f54702a4a7c1f6659d1d486a1565749353edf72
tommyniu <118729451+tommyniu@users.noreply.github.com>
2026-05-03 18:29:45 +0800

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,1 +1,6 @@
-
+export default {
+  async fetch(request, env, ctx) {
+    const workerUrl = "https://chatroom-server.ltc114514191.workers.dev";
+    return fetch(workerUrl + new URL(request.url).pathname + new URL(request.url).search, request);
+  },
+};
